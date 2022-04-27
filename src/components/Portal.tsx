de1e386207882868ddf43818dd27a520f1c6f240
Lochie Axon <lochieaxon@gmail.com>
2022-04-28 01:00:51 +1000

    Create Portal component

---
+++ src/components/Portal.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+const Portal = (props: any) => {
+  props = {
+    selector: '__FAMILYKIT__',
+    ...props,
+  };
+
+  const { selector, children } = props;
+
+  const ref = useRef<Element | null>(null);
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    const selectorPrefixed = '#' + selector.replace(/^#/, '');
+    ref.current = document.querySelector(selectorPrefixed);
+
+    if (!ref.current) {
+      const div = document.createElement('div');
+      div.setAttribute('id', selector);
+      document.body.appendChild(div);
+      ref.current = div;
+    }
+
+    setMounted(true);
+  }, [selector]);
+
+  if (!ref.current) return null;
+  return mounted ? createPortal(children, ref.current) : null;
+};
+
+export default Portal;
