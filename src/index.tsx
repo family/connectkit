ea1bde02300d6875d3532a8b93eccec432cb9a59
Lochie Axon <lochieaxon@gmail.com>
2022-04-28 01:00:32 +1000

    Remove global css files (keep fonts)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/reset.css';
-import './styles/index.css';
+import './styles/fonts.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
