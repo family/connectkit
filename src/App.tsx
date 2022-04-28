fff1b94cfd88e81299ba65df6b5a73be31c7e55c
Lochie Axon <lochieaxon@gmail.com>
2022-04-28 14:44:31 +1000

    Move walletconnect to top of provider list

--- src/App.tsx
+++ src/App.tsx
@@ -15,18 +15,20 @@ const infuraId = '34dfd61558944dbbb4a5187b330bed76';
 const client = createClient({
   autoConnect: true,
   connectors: [
-    new InjectedConnector(),
     new WalletConnectConnector({
       options: {
         infuraId,
         qrcode: false,
       },
     }),
-    // new CoinbaseWalletConnector({
-    //   options: {
-    //     appName: 'Family App',
-    //   },
-    // }),
+    new InjectedConnector(),
+    /*
+    new CoinbaseWalletConnector({
+      options: {
+        appName: 'Family App',
+      },
+    }),
+    */
   ],
   provider: (config) => {
     return new providers.InfuraProvider(config.chainId, infuraId);
