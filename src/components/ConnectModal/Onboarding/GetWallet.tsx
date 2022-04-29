b4ce8c0e1566c3b07dd23b7e82e68218fedc4037
Lochie Axon <lochieaxon@gmail.com>
2022-04-30 01:45:48 +1000

    Onboarding flows

---
+++ src/components/ConnectModal/Onboarding/GetWallet.tsx
@@ -0,0 +1,142 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+import {
+  ModalBody,
+  ModalContent,
+  ModalH1,
+  ModalHeading,
+} from '../../Modal/styles';
+import logos from '../../../assets/logos';
+
+const Container = styled(motion.div)`
+  max-width: 100%;
+  width: 320px;
+`;
+
+const WalletList = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+const WalletItem = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+const WalletButton = styled(motion.button)`
+  appearance: none;
+  user-select: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 9px;
+  height: 24px;
+  border-radius: 12px;
+  color: var(--body-color);
+  background: var(--body-background-secondary);
+  font-size: 13px;
+  font-weight: 600;
+  line-height: 16px;
+  text-transform: uppercase;
+  will-change: color, transform;
+  transition: background 200ms ease, transform 100ms ease;
+
+  &:hover {
+    background: var(--body-background-secondary-hover);
+  }
+  &:active {
+    transform: scale(0.96);
+  }
+`;
+const WalletInfo = styled(motion.div)`
+  width: 100%;
+`;
+const WalletName = styled(motion.div)`
+  color: var(--body-color);
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 20px;
+`;
+const WalletPlatform = styled(motion.div)`
+  color: var(--body-color-muted);
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+`;
+const WalletLogo = styled(motion.div)`
+  position: relative;
+  overflow: hidden;
+  display: block;
+  width: 40px;
+  height: 40px;
+  min-width: 40px;
+  min-height: 40px;
+  border-radius: 9.5px;
+  &:before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.05);
+  }
+  svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
+let wallets: any = [
+  {
+    name: 'MetaMask',
+    platform: 'Mobile & Desktop',
+    logo: logos.MetaMask,
+  },
+  {
+    name: 'Coinbase Wallet',
+    platform: 'Mobile & Desktop',
+    logo: logos.Coinbase,
+  },
+  {
+    name: 'Trust',
+    platform: 'Mobile',
+    logo: logos.Trust,
+  },
+];
+
+const GetWallet: React.FC = () => {
+  return (
+    <Container>
+      <ModalHeading>Get a Wallet</ModalHeading>
+      <ModalContent style={{ textAlign: 'left' }}>
+        <ModalH1>Some copy (maybe)</ModalH1>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus
+          vulputate donec risus etiam consectetur posuere volutpat.
+        </ModalBody>
+      </ModalContent>
+      <WalletList>
+        {wallets.map((wallet: any, i: number) => {
+          return (
+            <WalletItem
+              key={i}
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0, transition: { delay: i * 0.1 } }}
+            >
+              <WalletLogo>{wallet.logo}</WalletLogo>
+              <WalletInfo>
+                <WalletName>{wallet.name}</WalletName>
+                <WalletPlatform>{wallet.platform}</WalletPlatform>
+              </WalletInfo>
+              <WalletButton>Get</WalletButton>
+            </WalletItem>
+          );
+        })}
+      </WalletList>
+    </Container>
+  );
+};
+
+export default GetWallet;
---
+++ src/components/ConnectModal/Onboarding/Introduction.tsx
@@ -0,0 +1,190 @@
+import React from 'react';
+
+import styled, { keyframes } from 'styled-components';
+import { motion } from 'framer-motion';
+
+import {
+  ModalBody,
+  ModalContent,
+  ModalH1,
+  ModalHeading,
+} from '../../Modal/styles';
+import Button from '../../Button';
+import logos from '../../../assets/logos';
+import wave from '../../../assets/wave';
+import { routes, useContext } from '../../FamilyKit';
+
+const Container = styled(motion.div)`
+  max-width: 100%;
+  width: 320px;
+`;
+const Graphic = styled(motion.div)`
+  position: relative;
+  margin: 22px auto 18px;
+  height: 190px;
+  //pointer-events: none;
+  //user-select: none;
+`;
+const GraphicBackground = styled(motion.div)`
+  z-index: -1;
+  position: absolute;
+  inset: 0;
+  svg {
+    display: block;
+  }
+`;
+
+const logoIn = keyframes`
+  0%{
+    opacity:0;
+    transform:scale(0) translateY(40%);
+  }
+  100%{
+    opacity:1;
+    transform:none;
+  }
+`;
+const floatRotateA = keyframes`
+  0%,100%{ transform:none; }
+  50%{ transform:scale(1.05) translateY(-5%) rotate(-6deg); }
+`;
+const floatRotateB = keyframes`
+  0%,100%{ transform:none; }
+  50%{ transform:scale(0.96) translateY(2%) rotate(-2deg); }
+`;
+const floatRotateC = keyframes`
+  0%,100%{ transform:none; }
+  50%{ transform:scale(0.96) translateY(2%) rotate(2deg); }
+`;
+const floatRotateD = keyframes`
+  0%,100%{ transform:none; }
+  50%{ transform:scale(0.96) translateY(4%) rotate(6deg); }
+`;
+const floatRotateE = keyframes`
+  0%,100%{ transform:none; }
+  50%{ transform:scale(0.96) translateY(6%) rotate(-2deg); }
+`;
+const GraphicLogos = styled(motion.div)``;
+const LogoWrapper = styled(motion.div)`
+  position: absolute;
+  inset: 0;
+  animation: cubic-bezier(0.455, 0.03, 0.515, 0.955) infinite both;
+  animation-delay: inherit;
+`;
+const LogoGraphic = styled(motion.div)`
+  position: absolute;
+  overflow: hidden;
+  height: 58px;
+  width: 58px;
+  border-radius: 18px;
+  background: var(--body-background-secondary);
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05), 0 2px 20px 0 rgba(0, 0, 0, 0.03);
+
+  svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const GraphicLogo = styled(motion.div)`
+  position:absolute;
+  inset:0;
+  will-change: transform;
+  animation: ${logoIn} 1650ms cubic-bezier(0.19, 1, 0.22, 1) both;
+  &:nth-child(1){ z-index:2; animation-delay:0ms;  ${LogoWrapper}{ animation-name:${floatRotateA}; animation-duration:4000ms; } }
+  &:nth-child(2){ z-index:1; animation-delay:120ms; ${LogoWrapper}{ animation-name:${floatRotateB}; animation-duration:4300ms; } }
+  &:nth-child(3){ z-index:1; animation-delay:60ms; ${LogoWrapper}{ animation-name:${floatRotateC}; animation-duration:4100ms; } }
+  &:nth-child(4){ z-index:1; animation-delay:180ms; ${LogoWrapper}{ animation-name:${floatRotateD}; animation-duration:4600ms; } }
+  &:nth-child(5){ z-index:1; animation-delay:260ms; ${LogoWrapper}{ animation-name:${floatRotateE}; animation-duration:4800ms; } }
+  &:nth-child(1) ${LogoGraphic}{
+    top: 50%;
+    left: 50%;
+    width: 72px;
+    height: 72px;
+    transform: translate(-50%, -50%) rotate(4.28deg);
+  }
+  &:nth-child(2) ${LogoGraphic}{
+    top: 22%;
+    left: 23%;
+    transform: translate(-50%, -50%) rotate(-4.6deg);
+  }
+  &:nth-child(3) ${LogoGraphic}{
+    top: 15%;
+    right: 6%;
+    transform: translate(-50%, -50%) rotate(3.95deg);
+  }
+  &:nth-child(4) ${LogoGraphic}{
+    top: 82%;
+    left: 24%;
+    transform: translate(-50%, -50%) rotate(4.82deg);
+  }
+  &:nth-child(5) ${LogoGraphic}{
+    top: 80%;
+    right: 7%;
+    transform: translate(-50%, -50%) rotate(-7.51deg);
+  }
+}`;
+
+const Introduction: React.FC = () => {
+  const context = useContext();
+  return (
+    <Container>
+      <ModalHeading>Get a Wallet</ModalHeading>
+      <Graphic>
+        <GraphicLogos>
+          <GraphicLogo>
+            <LogoWrapper>
+              <LogoGraphic>{logos.Coinbase}</LogoGraphic>
+            </LogoWrapper>
+          </GraphicLogo>
+          <GraphicLogo>
+            <LogoWrapper>
+              <LogoGraphic>{logos.MetaMask}</LogoGraphic>
+            </LogoWrapper>
+          </GraphicLogo>
+          <GraphicLogo>
+            <LogoWrapper>
+              <LogoGraphic>{logos.Trust}</LogoGraphic>
+            </LogoWrapper>
+          </GraphicLogo>
+          <GraphicLogo>
+            <LogoWrapper>
+              <LogoGraphic>{logos.Argent}</LogoGraphic>
+            </LogoWrapper>
+          </GraphicLogo>
+          <GraphicLogo>
+            <LogoWrapper>
+              <LogoGraphic>{logos.imToken}</LogoGraphic>
+            </LogoWrapper>
+          </GraphicLogo>
+        </GraphicLogos>
+        <GraphicBackground
+          animate={{
+            opacity: [0, 1],
+            scale: [0.9, 1],
+            transition: { delay: 0.1, duration: 1 },
+          }}
+        >
+          {wave}
+        </GraphicBackground>
+      </Graphic>
+      <ModalContent>
+        <ModalH1>Start Exploring Web3</ModalH1>
+        <ModalBody>
+          Your wallet is the gateway to all things Ethereum, the magical
+          technology that makes it possible to explore web3.
+        </ModalBody>
+      </ModalContent>
+      <Button
+        onClick={() =>
+          context.setState({ open: true, route: routes.ONBOARDING_GETWALLET })
+        }
+      >
+        Choose Your First Wallet &rarr;
+      </Button>
+    </Container>
+  );
+};
+
+export default Introduction;
