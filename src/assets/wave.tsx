7d90597776808a0a9558851d427f6322a095b39f
Lochie Axon <lochieaxon@gmail.com>
2022-04-30 01:44:52 +1000

    Some graphic assets

---
+++ src/assets/wave.tsx
@@ -0,0 +1,153 @@
+export default (
+  <svg
+    width="296"
+    height="210"
+    viewBox="0 0 296 210"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <mask
+      id="mask0_977_713"
+      maskUnits="userSpaceOnUse"
+      x="-1"
+      y="11"
+      width="298"
+      height="188"
+    >
+      <path
+        d="M0 66.2756L20.6438 57.0284C54.5896 41.8227 93.4104 41.8227 127.356 57.0284L148 66.2756L168.644 75.5227C202.59 90.7285 241.41 90.7285 275.356 75.5227L296 66.2756"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 55.2118L20.6438 45.9646C54.5896 30.7589 93.4104 30.7589 127.356 45.9646L148 55.2118L168.644 64.459C202.59 79.6647 241.41 79.6647 275.356 64.459L296 55.2118"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 44.1476L20.6438 34.9004C54.5896 19.6947 93.4104 19.6947 127.356 34.9004L148 44.1476L168.644 53.3948C202.59 68.6005 241.41 68.6005 275.356 53.3948L296 44.1476"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 33.1476L20.6438 23.9004C54.5896 8.69472 93.4104 8.69472 127.356 23.9004L148 33.1476L168.644 42.3948C202.59 57.6005 241.41 57.6005 275.356 42.3948L296 33.1476"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 77.3398L20.6438 68.0926C54.5896 52.8869 93.4104 52.8869 127.356 68.0926L148 77.3398L168.644 86.587C202.59 101.793 241.41 101.793 275.356 86.587L296 77.3398"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 88.404L20.6438 79.1568C54.5896 63.9511 93.4104 63.9511 127.356 79.1568L148 88.404L168.644 97.6512C202.59 112.857 241.41 112.857 275.356 97.6512L296 88.404"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 99.4682L20.6438 90.221C54.5896 75.0153 93.4104 75.0153 127.356 90.221L148 99.4682L168.644 108.715C202.59 123.921 241.41 123.921 275.356 108.715L296 99.4682"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 132.66L20.6438 123.413C54.5896 108.207 93.4104 108.207 127.356 123.413L148 132.66L168.644 141.908C202.59 157.113 241.41 157.113 275.356 141.908L296 132.66"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 121.596L20.6438 112.349C54.5896 97.1432 93.4104 97.1432 127.356 112.349L148 121.596L168.644 130.843C202.59 146.049 241.41 146.049 275.356 130.843L296 121.596"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 110.532L20.6438 101.285C54.5896 86.079 93.4104 86.079 127.356 101.285L148 110.532L168.644 119.779C202.59 134.985 241.41 134.985 275.356 119.779L296 110.532"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 143.724L20.6438 134.477C54.5896 119.271 93.4104 119.271 127.356 134.477L148 143.724L168.644 152.971C202.59 168.177 241.41 168.177 275.356 152.971L296 143.724"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 154.788L20.6438 145.541C54.5896 130.335 93.4104 130.335 127.356 145.541L148 154.788L168.644 164.035C202.59 179.241 241.41 179.241 275.356 164.035L296 154.788"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 165.852L20.6438 156.605C54.5896 141.4 93.4104 141.4 127.356 156.605L148 165.852L168.644 175.1C202.59 190.305 241.41 190.305 275.356 175.1L296 165.852"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0 176.852L20.6438 167.605C54.5896 152.4 93.4104 152.4 127.356 167.605L148 176.852L168.644 186.1C202.59 201.305 241.41 201.305 275.356 186.1L296 176.852"
+        stroke="var(--body-background)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </mask>
+    <g mask="url(#mask0_977_713)">
+      <rect
+        x="295"
+        y="199"
+        width="295"
+        height="188"
+        transform="rotate(180 295 199)"
+        fill="url(#paint0_linear_977_713)"
+        fillOpacity="0.9"
+      />
+      <rect
+        x="296"
+        y="199"
+        width="296"
+        height="188"
+        transform="rotate(180 296 199)"
+        fill="url(#paint1_radial_977_713)"
+      />
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_977_713"
+        x1="295"
+        y1="311.888"
+        x2="590.172"
+        y2="279.64"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#F46D98" />
+        <stop offset="0.239583" stopColor="#DEA1E8" />
+        <stop offset="0.515625" stopColor="#7AA1F2" />
+        <stop offset="0.739583" stopColor="#A8ECDE" />
+        <stop offset="1" stopColor="#E8F17D" />
+      </linearGradient>
+      <radialGradient
+        id="paint1_radial_977_713"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(444 293) rotate(90.3291) scale(87.355 148.002)"
+      >
+        <stop
+          offset="0.1875"
+          stopColor="var(--body-background)"
+          stopOpacity="0"
+        />
+        <stop offset="1" stopColor="var(--body-background)" />
+      </radialGradient>
+    </defs>
+  </svg>
+);
