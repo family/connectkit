d76e19a66ab79f8977c83f50cba6358d1e30c8cf
Lochie Axon <lochieaxon@gmail.com>
2022-05-02 16:19:32 +1000

    browser detection

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,4 +7,24 @@ const truncateEthAddress = (address?: string) => {
   return `${match[1]}…${match[2]}`;
 };
 
-export { truncateEthAddress };
+const detectBrowser = () => {
+  const agent = window.navigator.userAgent.toLowerCase();
+  switch (true) {
+    case agent.indexOf('edge') > -1:
+      return 'edge';
+    case agent.indexOf('opr') > -1 && !!(<any>window).opr:
+      return 'opera';
+    case agent.indexOf('chrome') > -1 && !!(<any>window).chrome:
+      return 'chrome';
+    case agent.indexOf('trident') > -1:
+      return 'ie';
+    case agent.indexOf('firefox') > -1:
+      return 'firefox';
+    case agent.indexOf('safari') > -1:
+      return 'safari';
+    default:
+      return 'other';
+  }
+};
+
+export { truncateEthAddress, detectBrowser };
