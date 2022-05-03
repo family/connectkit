0e2099f82e2c38e9f83c7aa1f8f2084bff520516
Lochie Axon <lochieaxon@gmail.com>
2022-05-04 00:45:50 +1000

    fade out back arrow

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -185,7 +185,7 @@ const Modal: React.FC<ModalProps> = ({
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       exit={{ opacity: 0 }}
-                      transition={{ duration: 0.05 }}
+                      transition={{ duration: 0.1 }}
                     >
                       <BackIcon />
                     </BackButton>
