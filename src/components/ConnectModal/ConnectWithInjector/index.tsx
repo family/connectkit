2d80ca8dabc3264f6cf5d3b53136f1308f4497af
Lochie Axon <lochieaxon@gmail.com>
2022-05-05 03:21:42 +1000

    Disconnect icon

--- src/components/ConnectModal/ConnectWithInjector/index.tsx
+++ src/components/ConnectModal/ConnectWithInjector/index.tsx
@@ -71,6 +71,26 @@ const AlertIcon = ({ ...props }) => {
   );
 };
 
+const DisconnectIcon = ({ ...props }) => {
+  return (
+    <svg
+      width="15"
+      height="14"
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 0C1.79086 0 0 1.79086 0 4V10C0 12.2091 1.79086 14 4 14H6C6.55228 14 7 13.5523 7 13C7 12.4477 6.55228 12 6 12H4C2.89543 12 2 11.1046 2 10V4C2 2.89543 2.89543 2 4 2H6C6.55228 2 7 1.55228 7 1C7 0.447715 6.55228 0 6 0H4ZM11.7071 3.29289C11.3166 2.90237 10.6834 2.90237 10.2929 3.29289C9.90237 3.68342 9.90237 4.31658 10.2929 4.70711L11.5858 6H9.5H6C5.44772 6 5 6.44772 5 7C5 7.55228 5.44772 8 6 8H9.5H11.5858L10.2929 9.29289C9.90237 9.68342 9.90237 10.3166 10.2929 10.7071C10.6834 11.0976 11.3166 11.0976 11.7071 10.7071L14.7071 7.70711C15.0976 7.31658 15.0976 6.68342 14.7071 6.29289L11.7071 3.29289Z"
+        fill="currentColor"
+        fillOpacity="0.4"
+      />
+    </svg>
+  );
+};
+
 const TickIcon = ({ ...props }) => {
   return (
     <svg
@@ -381,7 +401,10 @@ const ConnectWithInjector: React.FC<{
                 </ModalH1>
                 <ModalBody>It is now safe to turn off your computer</ModalBody>
               </ModalContent>
-              <Button onClick={() => alert('TODO: Disconnect')}>
+              <Button
+                onClick={() => alert('TODO: Disconnect')}
+                icon={<DisconnectIcon />}
+              >
                 Disconnect
               </Button>
             </Content>
