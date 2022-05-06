939058ba626cd7c1ebf5348f42fefda94e627acb
Lochie Axon <lochieaxon@gmail.com>
2022-05-07 02:31:55 +1000

    replace detect-browser in utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { detect } from 'detect-browser';
+
 const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
 
 const truncateEthAddress = (address?: string) => {
@@ -8,23 +10,12 @@ const truncateEthAddress = (address?: string) => {
 };
 
 const detectBrowser = () => {
-  const agent = window.navigator.userAgent.toLowerCase();
-  switch (true) {
-    case agent.indexOf('edge') > -1:
-      return 'edge';
-    case agent.indexOf('opr') > -1 && !!(<any>window).opr:
-      return 'opera';
-    case agent.indexOf('chrome') > -1 && !!(<any>window).chrome:
-      return 'chrome';
-    case agent.indexOf('trident') > -1:
-      return 'ie';
-    case agent.indexOf('firefox') > -1:
-      return 'firefox';
-    case agent.indexOf('safari') > -1:
-      return 'safari';
-    default:
-      return 'other';
-  }
+  const browser = detect();
+  return browser?.name ?? '';
+};
+const detectOS = () => {
+  const browser = detect();
+  return browser?.os ?? '';
 };
 
-export { truncateEthAddress, detectBrowser };
+export { truncateEthAddress, detectBrowser, detectOS };
