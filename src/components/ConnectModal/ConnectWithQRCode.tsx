47505cb6b6d2cfb1125a4af3cc6724f7c96f99e1
Lochie Axon <lochieaxon@gmail.com>
2022-05-07 03:12:00 +1000

    add: external link icon

--- src/components/ConnectModal/ConnectWithQRCode.tsx
+++ src/components/ConnectModal/ConnectWithQRCode.tsx
@@ -24,6 +24,33 @@ const Container = styled(motion.div)`
   width: 295px;
 `;
 
+const ExternalLinkIcon = ({ ...props }) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M13 9.66667V12C13 13.6569 11.6569 15 10 15H4C2.34315 15 1 13.6569 1 12V6C1 4.34315 2.34315 3 4 3H6.33333"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeOpacity="0.3"
+    />
+    <path
+      d="M8 8L14 2L15 1M15 1V5.5M15 1H10.5"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeOpacity="0.3"
+    />
+  </svg>
+);
+
 const ConnectWithQRCode: React.FC<{
   connectorId: string;
   switchConnectMethod: (id?: string) => void;
@@ -153,7 +180,18 @@ const ConnectWithQRCode: React.FC<{
       </ModalContent>
 
       {connector.defaultConnect && ( // Open the default connector modal
-        <Button icon={connector.logo} onClick={connector.defaultConnect}>
+        <Button
+          icon={
+            <ExternalLinkIcon
+              style={{
+                transform: 'scale(0.75)',
+                left: 3,
+                top: 0,
+              }}
+            />
+          }
+          onClick={connector.defaultConnect}
+        >
           Open Default Modal
         </Button>
       )}
