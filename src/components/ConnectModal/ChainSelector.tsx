1e9ed5c7b4a7c0bbc5af134d576f0938233f67c5
Lochie Axon <lochieaxon@gmail.com>
2022-05-11 13:01:42 +1000

    Update: ChainSelector

--- src/components/ConnectModal/ChainSelector.tsx
+++ src/components/ConnectModal/ChainSelector.tsx
@@ -17,23 +17,30 @@ const DropdownWindow = styled(motion.div)`
   z-index: 2147483647;
   position: fixed;
   inset: 0;
-  pointer-events: none;
+`;
+const DropdownOverlay = styled(motion.div)`
+  position: absolute;
+  inset: 0;
 `;
 const DropdownContainer = styled(motion.div)`
   pointer-events: auto;
-  --shadow: 0px 2px 10px rgba(0, 0, 0, 0.08);
+  --shadow: 0px 2px 15px rgba(0, 0, 0, 0.15);
   z-index: 2147483647;
   position: absolute;
   top: 0;
   left: 0;
+  min-width: 292px;
   border-radius: 12px;
-  padding: 14px 16px;
-  font-size: 15px;
-  line-height: 18px;
-  font-weight: 400;
+  padding: 14px 20px 8px 16px;
   color: var(--tooltip-color);
   background: var(--tooltip-body);
   box-shadow: var(--shadow);
+`;
+const DropdownHeading = styled(motion.div)`
+  padding: 0 0 8px;
+  font-size: 15px;
+  line-height: 18px;
+  font-weight: 400;
 `;
 
 const SwitchChainButton = styled(motion.button)`
@@ -43,11 +50,11 @@ const SwitchChainButton = styled(motion.button)`
   user-select: none;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   border-radius: 15px;
   width: 52px;
   height: 30px;
-  padding: 2px 6px 2px 2px;
+  padding: 2px 6px 2px 3px;
   font-size: 16px;
   line-height: 19px;
   font-weight: 500;
@@ -58,6 +65,7 @@ const SwitchChainButton = styled(motion.button)`
   transition: transform 100ms ease, background-color 100ms ease;
   svg {
     position: relative;
+    display: block;
   }
 
   &:hover {
@@ -70,16 +78,20 @@ const SwitchChainButton = styled(motion.button)`
 const ChainIcon = styled(motion.div)`
   display: block;
   position: relative;
-  margin: 0 4px 0 0;
+  margin: 0 12px 0 0;
   border-radius: 12px;
+  overflow: hidden;
   width: 24px;
   height: 24px;
+  min-width: 24px;
+  min-height: 24px;
   background: var(--body-background);
   svg {
     width: 100%;
     height: auto;
   }
 `;
+const ChainButtons = styled(motion.div)``;
 const ChainButton = styled(motion.button)`
   appearance: none;
   cursor: pointer;
@@ -91,30 +103,38 @@ const ChainButton = styled(motion.button)`
   gap: 20px;
   width: 100%;
   border-radius: 11px;
-  margin: 6px 0 0;
+  margin: 0;
   padding: 8px 0;
   font-size: 15px;
-  line-height: 17px;
+  line-height: 18px;
   font-weight: 500;
   text-decoration: none;
   color: var(--body-color);
   background: none;
   white-space: nowrap;
   transition: transform 100ms ease, background-color 100ms ease;
-  span:first-child {
-  }
-  span:last-child {
-    color: var(--body-color-muted);
-  }
   &:active {
     transform: scale(0.98);
   }
+  &:disabled {
+    pointer-events: none;
+  }
+`;
+const ChainButtonStatus = styled(motion.div)`
+  color: var(--body-color-muted);
+  font-size: 15px;
+  line-height: 18px;
+  font-weight: 400;
+  span {
+    display: block;
+    position: relative;
+  }
 `;
 const ChainButtonBg = styled(motion.div)`
-  z-index: -1;
-  background: var(--body-background-secondary-hover);
+  opacity: 0.1;
+  background: var(--focus-color);
   position: absolute;
-  inset: 0 -8px;
+  inset: 0 -12px 0 -8px;
   border-radius: 12px;
 `;
 
@@ -139,7 +159,7 @@ const ChevronDown = ({ ...props }) => (
 
 const ChainSelector: React.FC = () => {
   const context = useContext();
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(!false);
   const [hover, setHover] = useState('');
   const {
     activeChain,
@@ -171,8 +191,8 @@ const ChainSelector: React.FC = () => {
         0
     )
       return;
-    const x = -10 + bounds.left;
-    const y = 8 + bounds.top + bounds.height;
+    const x = -12 + bounds.left;
+    const y = 10 + bounds.top + bounds.height;
     targetRef.current.style.left = `${x}px`;
     targetRef.current.style.top = `${y}px`;
   };
@@ -183,7 +203,7 @@ const ChainSelector: React.FC = () => {
       <Container>
         <SwitchChainButton ref={ref} onTap={() => setIsOpen(!isOpen)}>
           <ChainIcon>{ChainIcons.Ethereum}</ChainIcon>
-          <ChevronDown style={{ top: 1, left: -1 }} />
+          <ChevronDown style={{ top: 1, left: 1 }} />
         </SwitchChainButton>
       </Container>
       <Portal>
@@ -191,6 +211,7 @@ const ChainSelector: React.FC = () => {
           {context.open && isOpen && (
             <ResetContainer theme={context.theme}>
               <DropdownWindow>
+                <DropdownOverlay onClick={() => setIsOpen(false)} />
                 <DropdownContainer
                   ref={targetRef}
                   initial={'collapsed'}
@@ -223,46 +244,83 @@ const ChainSelector: React.FC = () => {
                     },
                   }}
                 >
-                  Switch Networks (WIP)
-                  {chains.map((x) => {
-                    const chain = supportedChains.filter(
-                      (c) => c.id === x.id
-                    )[0];
-                    return (
-                      <ChainButton
-                        disabled={!switchNetwork || x.id === activeChain?.id}
-                        key={x.id}
-                        onHoverStart={() => setHover(x.name)}
-                        onClick={() => switchNetwork?.(x.id)}
-                      >
-                        <span
-                          style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'flex-start',
-                          }}
+                  <DropdownHeading>Switch Networks</DropdownHeading>
+                  <ChainButtons>
+                    {chains.map((x) => {
+                      const chain = supportedChains.filter(
+                        (c) => c.id === x.id
+                      )[0];
+                      return (
+                        <ChainButton
+                          disabled={!switchNetwork || x.id === activeChain?.id}
+                          key={x.id}
+                          onHoverStart={() => setHover(x.name)}
+                          onClick={() => switchNetwork?.(x.id)}
                         >
-                          <ChainIcon>{chain.logo}</ChainIcon>
-                          {x.name}
-                        </span>
-                        <span>
-                          {isLoading &&
-                            pendingChainId === x.id &&
-                            ' Approve in Wallet'}
-                          {x.id === activeChain?.id && ' Connected'}
-                        </span>
-                        {hover === x.name && (
-                          <ChainButtonBg
-                            layoutId="hover"
-                            transition={{
-                              duration: 0.15,
-                              type: 'spring',
+                          <span
+                            style={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'flex-start',
                             }}
-                          />
-                        )}
-                      </ChainButton>
-                    );
-                  })}
+                          >
+                            <ChainIcon>{chain.logo}</ChainIcon>
+                            {x.name}
+                          </span>
+                          <ChainButtonStatus>
+                            <AnimatePresence exitBeforeEnter>
+                              {x.id === activeChain?.id && (
+                                <motion.span
+                                  style={{ color: 'var(--focus-color)' }}
+                                  initial={{ opacity: 0, y: 0, scale: 1 }}
+                                  animate={{ opacity: 1, y: 0, scale: 1 }}
+                                  exit={{ opacity: 0, y: 5, scale: 1 }}
+                                  transition={{
+                                    ease: [0.76, 0, 0.24, 1],
+                                    duration: 0.15,
+                                  }}
+                                >
+                                  Connected
+                                </motion.span>
+                              )}
+                              {isLoading && pendingChainId === x.id && (
+                                <motion.span
+                                  initial={{
+                                    opacity: 0,
+                                    y: -5,
+                                    scale: 1,
+                                  }}
+                                  animate={{ opacity: 1, y: 0, scale: 1 }}
+                                  exit={{ opacity: 0, y: 5, scale: 1 }}
+                                  transition={{
+                                    ease: [0.76, 0, 0.24, 1],
+                                    duration: 0.15,
+                                  }}
+                                >
+                                  Approve in Wallet
+                                </motion.span>
+                              )}
+                            </AnimatePresence>
+                          </ChainButtonStatus>
+                          {
+                            //hover === x.name && (
+                            x.id === activeChain?.id && (
+                              <ChainButtonBg
+                                //layoutId="hover"
+                                //layout="position"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 0.1 }}
+                                transition={{
+                                  duration: 0.15,
+                                  type: 'spring',
+                                }}
+                              />
+                            )
+                          }
+                        </ChainButton>
+                      );
+                    })}
+                  </ChainButtons>
                   <div>{error && error.message}</div>
                 </DropdownContainer>
               </DropdownWindow>
