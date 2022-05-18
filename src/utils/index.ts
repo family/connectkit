1459a5e28f0281b601dc3a70562671329e9b27c7
Lochie Axon <lochieaxon@gmail.com>
2022-05-19 00:25:41 +1000

    Fix: actually commit the util updates 🤡

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { detect } from 'detect-browser';
+import supportedConnectors from '../constants/supportedConnectors';
 
 const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
 
@@ -18,4 +19,58 @@ const detectOS = () => {
   return browser?.os ?? '';
 };
 
-export { truncateEthAddress, detectBrowser, detectOS };
+const isAndroid = () => {
+  return (
+    typeof navigator !== 'undefined' &&
+    /Android\s([0-9.]+)/.test(navigator.userAgent) // https://github.com/DamonOehlman/detect-browser/blob/master/src/index.ts
+  );
+};
+
+const isIOS = () => {
+  return (
+    typeof navigator !== 'undefined' &&
+    /Version\/([0-9._]+).*Mobile.*Safari.*/.test(navigator.userAgent) // https://github.com/DamonOehlman/detect-browser/blob/master/src/index.ts
+  );
+};
+
+const isMobile = () => {
+  return isAndroid() || isIOS();
+};
+
+const getWalletDownloadUri = (connectorId: string) => {
+  let url: string =
+    getMobileAppUri(connectorId) ?? getBrowserAppUri(connectorId);
+  return url;
+};
+const getMobileAppUri = (connectorId: string) => {
+  const c = supportedConnectors.filter((c) => c.id === connectorId)[0];
+  if (isIOS()) {
+    return c.appUrls?.ios ? c.appUrls.ios : '';
+  } else if (isAndroid()) {
+    return c.appUrls?.android ? c.appUrls.android : '';
+  }
+  return '';
+};
+const getBrowserAppUri = (connectorId: string) => {
+  const c = supportedConnectors.filter((c) => c.id === connectorId)[0];
+  const browser = detectBrowser();
+  /** WIP */
+  //ios: ['edge-ios', 'ios-webview', 'ios', 'fxios', 'crios'],
+  //android: ['android'],
+  switch (browser) {
+    case 'firefox':
+      return c.appUrls?.firefox ? c.appUrls.firefox : '';
+    case 'safari':
+      return c.appUrls?.safari ? c.appUrls.safari : '';
+    default:
+      return c.extensions?.chrome ? c.extensions?.chrome : '';
+  }
+};
+
+export {
+  truncateEthAddress,
+  isMobile,
+  detectBrowser,
+  detectOS,
+  getWalletDownloadUri,
+};
