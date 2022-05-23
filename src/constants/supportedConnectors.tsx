a4da18d30efa383760b9b5224259a58132eebd0f
Joseph Smith <jsmth.dev@gmail.com>
2022-05-23 17:58:55 +0100

    Added family docs

--- src/constants/supportedConnectors.tsx
+++ src/constants/supportedConnectors.tsx
@@ -7,68 +7,84 @@
  */
 
 import logos from './../assets/logos';
+let supportedConnectors: {
+  id: string;
+  name: string;
+  logo: any;
+  scannable: boolean;
+  extensions?: { [key: string]: string };
+  appUrls?: { [key: string]: string };
+  extensionIsInstalled?: () => any;
+  defaultConnect?: () => any;
+}[] = [];
 
-const { ethereum } = window;
+if (typeof window != 'undefined') {
+  const { ethereum } = window;
 
-interface IDictionary {
-  [index: string]: string;
+  interface IDictionary {
+    [index: string]: string;
+  }
+
+  // TODO: Define types
+  supportedConnectors = [
+    {
+      id: 'metaMask',
+      name: 'MetaMask',
+      logo: logos.MetaMask,
+      scannable: false,
+      // defaultConnect:  () => {},
+      extensions: {
+        chrome:
+          'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
+        firefox:
+          'https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/',
+        brave:
+          'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
+        edge: 'https://microsoftedge.microsoft.com/addons/detail/metamask/ejbalbakoplchlghecdalmeeeajnimhm?hl=en-US',
+      } as IDictionary,
+      appUrls: {
+        android: 'https://play.google.com/store/apps/details?id=io.metamask',
+        ios: 'https://apps.apple.com/us/app/metamask/id1438144202',
+        download: 'https://connect.family.co/download/metamask',
+      } as IDictionary,
+      extensionIsInstalled: () => {
+        return Boolean(ethereum && ethereum.isMetaMask);
+      },
+    },
+    {
+      id: 'walletConnect',
+      name: 'WalletConnect',
+      logo: logos.WalletConnect,
+      scannable: true,
+      defaultConnect: () => {},
+    },
+    {
+      id: 'coinbaseWallet',
+      name: 'Coinbase Wallet',
+      logo: logos.Coinbase,
+      scannable: true,
+      //defaultConnect: () => {},
+      extensions: {
+        chrome:
+          'https://chrome.google.com/webstore/detail/coinbase-wallet-extension/hnfanknocfeofbddgcijnmhnfnkdnaad',
+      } as IDictionary,
+      appUrls: {
+        android: 'https://play.google.com/store/apps/details?id=org.toshi',
+        ios: 'https://apps.apple.com/us/app/coinbase-wallet-store-crypto/id1278383455',
+        download: 'https://connect.family.co/download/coinbasewallet',
+      } as IDictionary,
+      extensionIsInstalled: () => {
+        return Boolean(
+          ethereum &&
+            (ethereum?.isCoinbaseWallet ||
+              (ethereum.providers &&
+                ethereum.providers.find(
+                  (provider) => provider.isCoinbaseWallet
+                )))
+        );
+      },
+    },
+  ];
 }
-// TODO: Define types
-const supportedConnectors = [
-  {
-    id: 'metaMask',
-    name: 'MetaMask',
-    logo: logos.MetaMask,
-    scannable: false,
-    // defaultConnect:  () => {},
-    extensions: {
-      chrome:
-        'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
-      firefox: 'https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/',
-      brave:
-        'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
-      edge: 'https://microsoftedge.microsoft.com/addons/detail/metamask/ejbalbakoplchlghecdalmeeeajnimhm?hl=en-US',
-    } as IDictionary,
-    appUrls: {
-      android: 'https://play.google.com/store/apps/details?id=io.metamask',
-      ios: 'https://apps.apple.com/us/app/metamask/id1438144202',
-      download: 'https://connect.family.co/download/metamask',
-    } as IDictionary,
-    extensionIsInstalled: () => {
-      return Boolean(ethereum && ethereum.isMetaMask);
-    },
-  },
-  {
-    id: 'walletConnect',
-    name: 'WalletConnect',
-    logo: logos.WalletConnect,
-    scannable: true,
-    defaultConnect: () => {},
-  },
-  {
-    id: 'coinbaseWallet',
-    name: 'Coinbase Wallet',
-    logo: logos.Coinbase,
-    scannable: true,
-    //defaultConnect: () => {},
-    extensions: {
-      chrome:
-        'https://chrome.google.com/webstore/detail/coinbase-wallet-extension/hnfanknocfeofbddgcijnmhnfnkdnaad',
-    } as IDictionary,
-    appUrls: {
-      android: 'https://play.google.com/store/apps/details?id=org.toshi',
-      ios: 'https://apps.apple.com/us/app/coinbase-wallet-store-crypto/id1278383455',
-      download: 'https://connect.family.co/download/coinbasewallet',
-    } as IDictionary,
-    extensionIsInstalled: () => {
-      return Boolean(
-        ethereum &&
-          (ethereum?.isCoinbaseWallet ||
-            (ethereum.providers &&
-              ethereum.providers.find((provider) => provider.isCoinbaseWallet)))
-      );
-    },
-  },
-];
 
 export default supportedConnectors;
