0deee3db5280e7c0a8ed02dc0c78246e77354585
Joseph Smith <jsmth.dev@gmail.com>
2022-05-23 13:32:27 +0100

    Fixed build issues and dev issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 const packageJson = require('./package.json');
 
@@ -28,7 +28,7 @@ export default [
         useTsconfigDeclarationDir: true,
         exclude: 'node_modules/**',
       }),
-      visualizer(),
+      // visualizer(),
     ],
   },
 ];
