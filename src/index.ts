e0dbaec9483900ceb287344674249ce1b3c8b6ad
Lochie Axon <lochieaxon@gmail.com>
2022-06-09 01:22:34 +1000

    Move: QA out of the modal and into tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-export { default as ConnectKitModal } from './components/ConnectModal';
-export { ConnectKitProvider } from './components/ConnectKit';
-export { ConnectKitButton } from './components/ConnectButton';
-export { default as QA } from './components/QA';
-export { default as supportedConnectors } from './constants/supportedConnectors';
 export * as Types from './types';
-
 export { default as getDefaultClient } from './defaultClient';
 
+export { ConnectKitProvider } from './components/ConnectKit';
+export { ConnectKitButton } from './components/ConnectButton';
+
+// TODO: Make this private
+export { default as supportedConnectors } from './constants/supportedConnectors';
 export { default as ConnectKitModalDemo } from './components/ConnectModal/demo';
