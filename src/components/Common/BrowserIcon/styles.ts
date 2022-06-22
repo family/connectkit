1589734dd4a4802e8db6d1b4c22f5a544ff8f266
Lochie Axon <lochieaxon@gmail.com>
2022-06-22 14:53:56 +1000

    Fix: browser icon size in button

--- src/components/Common/BrowserIcon/styles.ts
+++ src/components/Common/BrowserIcon/styles.ts
@@ -1,4 +1,3 @@
-
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
@@ -6,8 +5,10 @@ export const BrowserIconContainer = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 32px;
-  height: 32px;
+  max-width: 32px;
+  max-height: 32px;
+  width: 100%;
+  height: 100%;
   svg {
     display: block;
     width: 100%;
