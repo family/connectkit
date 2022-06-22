71f68da2b183d4247184ac01a06c25100243e0ac
Lochie Axon <lochieaxon@gmail.com>
2022-06-23 00:43:27 +1000

    Don't include metamask and coinbase in the defaultWallets if they are present as default connectors

--- src/wallets/useDefaultWallets.tsx
+++ src/wallets/useDefaultWallets.tsx
@@ -1,21 +1,33 @@
 import { getWallets } from '.';
 import { WalletProps } from './wallet';
 
-import { useNetwork } from 'wagmi';
-
-const defaultWallets = [
-  'metaMask',
-  'coinbaseWallet',
-  'rainbow',
-  'argent',
-  'trust',
-  'ledger',
-  'imToken',
-  'brave',
-];
+import { useNetwork, useConnect } from 'wagmi';
 
 function useDefaultWallets(): WalletProps[] | any {
   const { chains } = useNetwork();
+  const { connectors } = useConnect();
+
+  let defaultWallets = [];
+
+  // If missing metamask or coinbasewallet connector, add them to this list
+  if (!connectors.find((c) => c.id === 'metaMask'))
+    defaultWallets.push('metaMask');
+
+  // TODO: Add coinbasewallet connector
+
+  if (!connectors.find((c) => c.id === 'coinbaseWallet'))
+    defaultWallets.push('coinbaseWallet');
+
+  // Add default wallets
+  defaultWallets.push(
+    'rainbow',
+    'argent',
+    'trust',
+    'ledger',
+    'imToken',
+    'brave'
+  );
+
   const wallets = getWallets({ chains });
   return wallets.filter((wallet) => defaultWallets.includes(wallet.id));
 }
