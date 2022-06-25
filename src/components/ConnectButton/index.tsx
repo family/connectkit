cdfa422960ced0ea4d39ca18a435615a488d0ef2
Dennis Jin <mail@dennisjin.com>
2022-06-26 01:47:10 +0200

    Another button adjustment

--- src/components/ConnectButton/index.tsx
+++ src/components/ConnectButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 import { useConnect, useAccount, useEnsName, useNetwork } from 'wagmi';
 import { truncateEthAddress } from './../../utils';
 import { ResetContainer } from '../../styles';
@@ -11,50 +11,26 @@ import useMeasure from 'react-use-measure';
 
 const contentVariants: Variants = {
   initial: {
+    //willChange: 'transform,opacity',
     zIndex: 2,
     opacity: 0,
-    // scale: 1.1,
-    x: 0.1,
+    // y: -4,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
     transition: {
-      duration: 0,
+      duration: 0.3,
     },
   },
-  animate: {
-    opacity: 1,
-    y: 0,
-    // scale: 1,
-    x: 0.1,
-  },
   exit: {
     zIndex: 1,
     opacity: 0,
-    // scale: 1.1,
-    x: 0.1,
+    // y: 4,
     pointerEvents: 'none',
     position: 'absolute',
-  },
-};
-
-const addressVariants: Variants = {
-  initial: {
-    zIndex: 2,
-    opacity: 0,
-    // scale: 0.8,
-    x: 0.1,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
-    // scale: 1,
-    x: 0.1,
-  },
-  exit: {
-    zIndex: 1,
-    opacity: 0,
-    // scale: 0.8,
-    x: 0.1,
-    pointerEvents: 'none',
-    position: 'absolute',
+    transition: { duration: 0.2 },
   },
 };
 
@@ -127,8 +103,6 @@ export function ConnectKitButton() {
   const context = useContext();
   const containerRef = useRef<any>(null);
   const [contentRef, bounds] = useMeasure({ offsetSize: true });
-  const [width, setWidth] = useState<number>(0);
-  const [isInitial, setIsInitial] = useState<boolean>(true);
 
   const { activeChain } = useNetwork();
   const { data: account } = useAccount();
@@ -153,11 +127,7 @@ export function ConnectKitButton() {
   const refreshLayout = () => {
     if (!containerRef.current || bounds.width === 0) return;
     //containerRef.current.style.setProperty('--height', `${bounds.height}px`);
-    // containerRef.current.style.setProperty('--width', `${bounds.width}px`);
-    setWidth(bounds.width);
-    setTimeout(() => {
-      setIsInitial(false);
-    }, 0);
+    containerRef.current.style.setProperty('--width', `${bounds.width}px`);
   };
   useIsomorphicLayoutEffect(refreshLayout, [bounds]);
 
@@ -168,57 +138,27 @@ export function ConnectKitButton() {
       $useTheme={context.theme}
       $customTheme={context.customTheme}
       ref={containerRef}
-      initial={false}
       style={{
-        // display: 'inline-block',
-        // width: 300,
-        // width: bounds.width === 0 ? 'fit-content' : width < 107 ? 107 : width,
-
-        display: 'inline-flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-      animate={{
-        width: width < 107 ? 137 : width + 30,
-        // maxWidth:
-        // bounds.width === 0 ? 'none' : 'calc(var(--width, 107px) + 30px)',
-        // maxWidth: bounds.width === 0 ? '100%' : (width || 107) + 30,
-      }}
-      transition={{
-        ease: [0.25, 1, 0.5, 1],
-        duration: isInitial ? 0 : 0.22,
+        display: 'inline-block',
+        width: bounds.width === 0 ? 'fit-content' : '100%',
+        maxWidth:
+          bounds.width === 0 ? 'none' : 'calc(var(--width, 107px) + 30px)',
+        transition: 'max-width 100ms ease',
       }}
     >
-      <Button
-        initial={false}
-        onClick={show}
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <Button onClick={show}>
         <AnimatePresence initial={false}>
           {activeChain?.unsupported ? (
             <>Wrong network</>
           ) : (
-            <div
-              ref={contentRef}
-              style={{
-                width: 'fit-content',
-              }}
-            >
+            <div ref={contentRef} style={{ width: 'fit-content' }}>
               {account?.address ? (
                 <TextContainer
                   key="connected"
                   initial={'initial'}
                   animate={'animate'}
                   exit={'exit'}
-                  variants={addressVariants}
-                  transition={{
-                    ease: [0.25, 1, 0.5, 1],
-                    duration: isInitial ? 0 : 0.22,
-                  }}
+                  variants={contentVariants}
                 >
                   <IconContainer>
                     <Avatar size={24} address={account?.address} />
@@ -232,10 +172,6 @@ export function ConnectKitButton() {
                   animate={'animate'}
                   exit={'exit'}
                   variants={contentVariants}
-                  transition={{
-                    ease: [0.25, 1, 0.5, 1],
-                    duration: isInitial ? 0 : 0.22,
-                  }}
                 >
                   Connect Wallet
                 </TextContainer>
