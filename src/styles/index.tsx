eb8f54a4422817aedfada5d94ed0d85e655e325e
Lochie Axon <lochieaxon@gmail.com>
2022-06-30 18:29:51 +1000

    retro theme + choose a theme buttons

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -364,6 +364,61 @@ const themeColors = {
     '--qr-dot-color': '#ffffff',
     '--qr-border-color': '#3d3d3d',
   },
+
+  retro: {
+    '--border-radius': '8px',
+    '--connectbutton-color': '#ffffff',
+    '--connectbutton-background': '#2B2B2B',
+    '--connectbutton-background-hover': '#333333',
+    '--connectbutton-background-active': '#4D4D4D',
+
+    '--ck-primary-button-color': '#373737',
+    '--ck-primary-button-background': '#ffffff',
+    '--ck-primary-button-box-shadow':
+      '-4px 4px 0px #000000, inset 0 0 0 2px #000000',
+    '--ck-primary-button-border-radius': '8px',
+
+    '--ck-primary-button-hover-color': '#373737',
+    '--ck-primary-button-hover-background': '#F3EDE8',
+    '--ck-primary-button-hover-box-shadow':
+      '0px 0px 0px #000000, inset 0 0 0 2px #000000',
+    '--ck-primary-button-hover-border-radius': '8px',
+    '--ck-primary-button-active-border-radius': '8px',
+
+    '--ck-secondary-button-background': '#444555',
+    '--ck-secondary-button-hover-background': '#444555',
+
+    '--focus-color': '#98D79C',
+
+    '--overlay-background': '#84787A',
+    '--body-color': '#373737',
+    '--body-color-muted': '#373737',
+    '--body-color-muted-hover': '#000000',
+    '--body-background': '#EBE1D8',
+    '--body-background-transparent': 'rgba(0,0,0,0)',
+    '--body-background-secondary': '#333333',
+    '--body-background-secondary-hover': '#4D4D4D',
+    '--body-background-secondary-hover-outline': '#373737',
+    '--body-background-tertiary': '#333333',
+    '--body-action-color': '#373737',
+    '--body-divider': '#373737',
+    '--body-color-danger': '#FF4E4E',
+
+    '--ck-modal-box-shadow': '-10px 10px 0px #000000, inset 0 0 0 2px #000000',
+
+    '--copytoclipboard-stroke': '#555555',
+
+    '--tooltip-background': '#2B2B2B',
+    '--tooltip-background-secondary': '#333333',
+    '--tooltip-color': '#999999',
+    '--tooltip-shadow': '0px 2px 10px rgba(0, 0, 0, 0.08)',
+
+    '--spinner-color': '#98D79C',
+
+    '--qr-dot-color': '#ffffff',
+    '--qr-border-color': '#3d3d3d',
+  },
+
   lightMobile: {
     /*
     '--body-background': '#F8F8F8',
@@ -420,6 +475,7 @@ const themes = {
   darkMobile: createCssColors(themeColors.darkMobile),
   web95: createCssColors(themeColors.web95),
   alien: createCssColors(themeColors.alien),
+  retro: createCssColors(themeColors.retro),
 };
 const globals = {
   brand: createCssVars(themeGlobals.brand),
@@ -461,6 +517,8 @@ export const ResetContainer = styled(motion.div)<{
         return globalsDark;
       case 'alien':
         return globalsDark;
+      case 'retro':
+        return globalsDark;
       default:
         return css`
           ${globalsLight}
@@ -483,6 +541,8 @@ export const ResetContainer = styled(motion.div)<{
         return themes.web95;
       case 'alien':
         return themes.alien;
+      case 'retro':
+        return themes.retro;
       default:
         return css`
           ${themes.light}
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type Theme = 'light' | 'dark' | 'web95' | 'alien' | 'auto';
+export type Theme = 'light' | 'dark' | 'web95' | 'alien' | 'retro' | 'auto';
 export type Mode = 'light' | 'dark' | 'auto';
 export type CustomTheme = any;
 export type Languages = 'en';
