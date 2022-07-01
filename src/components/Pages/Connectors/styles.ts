a7a970487c0014d826affc9dab5f938bf49a4a31
Lochie Axon <lochieaxon@gmail.com>
2022-07-02 01:29:32 +1000

    semibold connector buttons

--- src/components/Pages/Connectors/styles.ts
+++ src/components/Pages/Connectors/styles.ts
@@ -91,7 +91,7 @@ export const ConnectorButton = styled(motion.button)`
   width: 100%;
   height: 64px;
   font-size: 17px;
-  font-weight: 500;
+  font-weight: 600;
   line-height: 20px;
   text-align: var(--body-button-text-align, left);
   transition: 180ms ease;
