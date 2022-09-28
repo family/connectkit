7b37e893f96a875332b1cb28854d9097bab11dc5
Joseph Smith <jsmth.dev@gmail.com>
2022-09-28 16:00:27 +0100

    Removed: Unneeded dev import

--- packages/connectkit/rollup.config.dev.js
+++ packages/connectkit/rollup.config.dev.js
@@ -1,7 +1,6 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 
 const packageJson = require('./package.json');
 
