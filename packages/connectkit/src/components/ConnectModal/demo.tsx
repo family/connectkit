c93ff1953343c5daf1592fe5e8626d3a8d8a0e4c
Lochie Axon <lochieaxon@gmail.com>
2022-11-18 10:45:45 +1100

    fix: demo modal animation issues

--- packages/connectkit/src/components/ConnectModal/demo.tsx
+++ packages/connectkit/src/components/ConnectModal/demo.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useAccount } from 'wagmi';
+import { useAccount, useNetwork } from 'wagmi';
 import { routes, useContext } from '../ConnectKit';
 import { CustomTheme, Languages, Theme, Mode } from '../../types';
 import Modal from '../Common/Modal';
@@ -98,12 +98,49 @@ const ConnectModal: React.FC<{
 }) => {
   const context = useContext();
   const { isConnected } = useAccount();
+  const { chain } = useNetwork();
+
+  const closeable = !chain?.unsupported;
+
+  const showBackButton =
+    !chain?.unsupported &&
+    context.route !== routes.CONNECTORS &&
+    context.route !== routes.PROFILE;
+
+  const showInfoButton =
+    !chain?.unsupported && context.route !== routes.PROFILE;
+
+  const onBack = () => {
+    if (context.route === routes.SIGNINWITHETHEREUM) {
+      context.setRoute(routes.PROFILE);
+    } else if (context.route === routes.SWITCHNETWORKS) {
+      context.setRoute(routes.PROFILE);
+    } else if (context.route === routes.DOWNLOAD) {
+      context.setRoute(routes.CONNECT);
+    } else {
+      context.setRoute(routes.CONNECTORS);
+    }
+  };
+
+  const pages: any = {
+    onboarding: <Onboarding />,
+    about: <About />,
+    download: <DownloadApp connectorId={context.connector} />,
+    connectors: <Connectors />,
+    mobileConnectors: <MobileConnectors />,
+    connect: <ConnectUsing connectorId={context.connector} />,
+    profile: <Profile closeModal={() => setIsOpen(false)} />,
+    switchNetworks: <SwitchNetworks />,
+    signInWithEthereum: <SignInWithEthereum />,
+  };
 
   const ref = useRef<HTMLDivElement | null>(null);
   const cursorRef = useRef<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(open ?? false);
 
   useEffect(() => {
+    if (open)
+      context.setRoute(isConnected ? routes.PROFILE : routes.CONNECTORS);
     setIsOpen(open ?? false);
   }, [open]);
 
@@ -144,33 +181,6 @@ const ConnectModal: React.FC<{
         ref.current.classList.add('shake');
       }
     }
-  };
-
-  const showBackButton =
-    context.route !== routes.CONNECTORS && context.route !== routes.PROFILE;
-
-  const onBack = () => {
-    if (context.route === routes.SIGNINWITHETHEREUM) {
-      context.setRoute(routes.PROFILE);
-    } else if (context.route === routes.SWITCHNETWORKS) {
-      context.setRoute(routes.PROFILE);
-    } else if (context.route === routes.DOWNLOAD) {
-      context.setRoute(routes.CONNECT);
-    } else {
-      context.setRoute(routes.CONNECTORS);
-    }
-  };
-
-  const pages: any = {
-    onboarding: <Onboarding />,
-    about: <About />,
-    download: <DownloadApp connectorId={context.connector} />,
-    connectors: <Connectors />,
-    mobileConnectors: <MobileConnectors />,
-    connect: <ConnectUsing connectorId={context.connector} />,
-    profile: <Profile closeModal={() => setIsOpen(false)} />,
-    switchNetworks: <SwitchNetworks />,
-    signInWithEthereum: <SignInWithEthereum />,
   };
 
   useEffect(() => {
@@ -226,15 +236,13 @@ const ConnectModal: React.FC<{
         )}
         <Modal
           demo={{ theme: theme, customTheme: customTheme, mode: mode }}
-          onClose={onModalClose}
+          onClose={closeable ? onModalClose : undefined}
           positionInside={inline}
           open={isOpen}
           pages={pages}
           pageId={context.route}
           onInfo={
-            context.route !== routes.PROFILE
-              ? () => context.setRoute(routes.ABOUT)
-              : undefined
+            showInfoButton ? () => context.setRoute(routes.ABOUT) : undefined
           }
           onBack={showBackButton ? onBack : undefined}
         />
