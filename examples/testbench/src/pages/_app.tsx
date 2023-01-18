dc6a7613029d8d0e3b314a9848df167d1730db5d
Lochie Axon <lochieaxon@gmail.com>
2023-01-19 01:53:41 +1100

    walletconnect 2.0

--- examples/testbench/src/pages/_app.tsx
+++ examples/testbench/src/pages/_app.tsx
@@ -15,6 +15,12 @@ const client = createClient(
     appIcon: '/app.png',
     infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
     alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    walletConnectOptions: false
+      ? {
+          version: '2',
+          projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+        }
+      : undefined,
   })
 );
 
--- packages/connectkit/src/defaultClient.ts
+++ packages/connectkit/src/defaultClient.ts
@@ -1,9 +1,5 @@
-import {
-  Connector,
-  configureChains,
-  ChainProviderFn,
-} from 'wagmi';
-import { Chain, mainnet, polygon, optimism, arbitrum } from 'wagmi/chains'
+import { Connector, configureChains, ChainProviderFn } from 'wagmi';
+import { Chain, mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 import { Provider } from '@wagmi/core';
 
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
@@ -21,16 +17,22 @@ let globalAppIcon: string;
 export const getAppName = () => globalAppName;
 export const getAppIcon = () => globalAppIcon;
 
-const defaultChains = [
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-];
+const defaultChains = [mainnet, polygon, optimism, arbitrum];
+
+type WalletConnectOptionsProps =
+  | {
+      version: '2';
+      projectId: string;
+    }
+  | {
+      version: '1';
+    }
+  | undefined;
 
 type DefaultConnectorsProps = {
   chains?: Chain[];
   appName: string;
+  walletConnectOptions?: WalletConnectOptionsProps;
 };
 
 type DefaultClientProps = {
@@ -45,6 +47,7 @@ type DefaultClientProps = {
   webSocketProvider?: any;
   enableWebSocketProvider?: boolean;
   stallTimeout?: number;
+  walletConnectOptions?: WalletConnectOptionsProps;
 };
 
 type ConnectKitClientProps = {
@@ -54,7 +57,21 @@ type ConnectKitClientProps = {
   webSocketProvider?: any;
 };
 
-const getDefaultConnectors = ({ chains, appName }: DefaultConnectorsProps) => {
+const getDefaultConnectors = ({
+  chains,
+  appName,
+  walletConnectOptions,
+}: DefaultConnectorsProps) => {
+  const wcOpts: WalletConnectOptionsProps =
+    walletConnectOptions?.version === '2' && walletConnectOptions?.projectId
+      ? {
+          version: '2',
+          projectId: walletConnectOptions.projectId, // WC 2.0 requires a project ID (get one here: https://cloud.walletconnect.com/sign-in)
+        }
+      : {
+          version: '1',
+        };
+
   return [
     new MetaMaskConnector({
       chains,
@@ -75,6 +92,7 @@ const getDefaultConnectors = ({ chains, appName }: DefaultConnectorsProps) => {
       chains,
       options: {
         qrcode: false,
+        ...wcOpts,
       },
     }),
     new InjectedConnector({
@@ -104,6 +122,7 @@ const defaultClient = ({
   stallTimeout,
   webSocketProvider,
   enableWebSocketProvider,
+  walletConnectOptions,
 }: DefaultClientProps) => {
   globalAppName = appName;
   if (appIcon) globalAppIcon = appIcon;
@@ -134,7 +153,12 @@ const defaultClient = ({
   const connectKitClient: ConnectKitClientProps = {
     autoConnect,
     connectors:
-      connectors ?? getDefaultConnectors({ chains: configuredChains, appName }),
+      connectors ??
+      getDefaultConnectors({
+        chains: configuredChains,
+        appName,
+        walletConnectOptions,
+      }),
     provider: provider ?? configuredProvider,
     webSocketProvider: enableWebSocketProvider // Removed by default, breaks if used in Next.js – "unhandledRejection: Error: could not detect network"
       ? webSocketProvider ?? configuredWebSocketProvider
