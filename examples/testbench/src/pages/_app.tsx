cfb2fba59de11f7a0301eff754c6ed0a256e5038
Lochie Axon <lochieaxon@gmail.com>
2023-01-19 02:07:53 +1100

    add wc 2.0 config to testbench

--- examples/testbench/src/pages/_app.tsx
+++ examples/testbench/src/pages/_app.tsx
@@ -15,12 +15,10 @@ const client = createClient(
     appIcon: '/app.png',
     infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
     alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
-    walletConnectOptions: false
-      ? {
-          version: '2',
-          projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
-        }
-      : undefined,
+    walletConnectOptions: {
+      version: '2',
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+    },
   })
 );
 
