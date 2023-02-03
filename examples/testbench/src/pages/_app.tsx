fdc7004e91b5cc0a67ab1847f3c85c208294bd6d
Lochie Axon <lochieaxon@gmail.com>
2023-02-04 01:07:06 +1100

    testbench: stylesheet in right file

--- examples/testbench/src/pages/_app.tsx
+++ examples/testbench/src/pages/_app.tsx
@@ -52,10 +52,6 @@ function MyApp(appProps: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
-        <link
-          href="https://fonts.cdnfonts.com/css/pt-root-ui"
-          rel="stylesheet"
-        />
       </Head>
       <WagmiConfig client={client}>
         <TestBenchProvider
