bcbf56d04c49b06833622e8829be1d2417e22f69
Lochie Axon <lochieaxon@gmail.com>
2023-02-04 01:10:15 +1100

    z-index on w3m

--- packages/connectkit/src/hooks/useDefaultWalletConnect.tsx
+++ packages/connectkit/src/hooks/useDefaultWalletConnect.tsx
@@ -8,6 +8,11 @@ export function useDefaultWalletConnect() {
   const { connectAsync, connectors } = useConnect();
   return {
     openDefaultWalletConnect: async () => {
+      //add modal styling because wagmi does not let you add styling to the modal
+      const w3mcss = document.createElement('style');
+      w3mcss.innerHTML = `w3m-modal{ --w3m-modal-z-index:2147483647; }`;
+      document.head.appendChild(w3mcss);
+
       const c: Connector<any, any> | undefined = connectors.find((c) =>
         isWalletConnectConnector(c.id)
       );
@@ -22,6 +27,9 @@ export function useDefaultWalletConnect() {
         } catch (err) {
           console.log('WalletConnect', err);
         }
+
+        // remove modal styling
+        document.head.removeChild(w3mcss);
       } else {
         console.log('No WalletConnect connector available');
       }
