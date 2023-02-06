5d04ebe0974bf5a7e5dbccab24d6231e5e216c72
Lochie Axon <lochieaxon@gmail.com>
2023-02-06 22:45:28 +1100

    wcv2 tester

--- examples/testbench/src/pages/_app.tsx
+++ examples/testbench/src/pages/_app.tsx
@@ -7,21 +7,7 @@ import { mainnet, polygon } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 import { TestBenchProvider, useTestBench } from '../TestbenchProvider';
 import { siwe } from '../siwe';
-
-const client = createClient(
-  getDefaultClient({
-    //chains: [mainnet, polygon],
-    appName: 'ConnectKit testbench',
-    appIcon: '/app.png',
-    infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
-    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
-    // WalletConnect 2.0
-    walletConnectOptions: {
-      version: '2',
-      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
-    },
-  })
-);
+import { useState } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
   const { theme, mode, options, customTheme } = useTestBench();
@@ -43,6 +29,28 @@ function App({ Component, pageProps }: AppProps) {
   );
 }
 function MyApp(appProps: AppProps) {
+  const [version, setVersion] = useState('1');
+
+  const client = createClient(
+    getDefaultClient({
+      //chains: [mainnet, polygon],
+      appName: 'ConnectKit testbench',
+      appIcon: '/app.png',
+      infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+      alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+      // WalletConnect 2.0
+      walletConnectOptions:
+        version === '1'
+          ? {
+              version: '1',
+            }
+          : {
+              version: '2',
+              projectId: process.env
+                .NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+            },
+    })
+  );
   return (
     <>
       <Head>
@@ -57,6 +65,14 @@ function MyApp(appProps: AppProps) {
         <TestBenchProvider
         //customTheme={{ '--ck-font-family': 'monospace' }}
         >
+          <div style={{ position: 'absolute', top: 0, right: 0 }}>
+            <button onClick={() => setVersion('1')} disabled={version === '1'}>
+              Use WalletConnect V1
+            </button>
+            <button onClick={() => setVersion('2')} disabled={version === '2'}>
+              Use WalletConnect V2
+            </button>
+          </div>
           <App {...appProps} />
         </TestBenchProvider>
       </WagmiConfig>
