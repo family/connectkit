18b0a0a7a0e659f43b11958507eaf18029b2e65c
Lochie Axon <lochieaxon@gmail.com>
2023-02-06 16:31:22 +1100

    export useIsMounted hook

--- packages/connectkit/src/index.ts
+++ packages/connectkit/src/index.ts
@@ -13,6 +13,9 @@ export { default as SIWEButton } from './components/Standard/SIWE';
 export { default as Avatar } from './components/Common/Avatar';
 export { default as ChainIcon } from './components/Common/Chain';
 
+// Hooks
+export { default as useIsMounted } from './hooks/useIsMounted'; // Useful for apps that use SSR
+
 // TODO: Make this private
 export { default as supportedConnectors } from './constants/supportedConnectors';
 export { default as ConnectKitModalDemo } from './components/ConnectModal/demo';
