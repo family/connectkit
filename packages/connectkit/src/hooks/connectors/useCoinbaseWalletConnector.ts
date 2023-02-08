bf677526e95075881da96c03a47f7471ca86a376
Lochie Axon <lochieaxon@gmail.com>
2023-02-09 00:27:57 +1100

    fix cbw connector

--- packages/connectkit/src/hooks/connectors/useCoinbaseWalletConnector.ts
+++ packages/connectkit/src/hooks/connectors/useCoinbaseWalletConnector.ts
@@ -1,26 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Connector, useConnectors } from './../useConnectors';
 
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
-
+// CoinbaseWalletConnector requires using the connector directly from wagmi's useConnect Hook (useConnectors) or else it won't connect properly.
 export const useCoinbaseWalletConnector = () => {
   const [connector, setConnector] = useState<Connector | undefined>(undefined);
-
   const connectors = useConnectors();
-  const coinbaseWallet = connectors.find((c) => c.id === 'coinbaseWallet');
 
   useEffect(() => {
-    if (coinbaseWallet) {
-      const config = {
-        ...coinbaseWallet,
-        options: {
-          ...coinbaseWallet.options,
-          headlessMode: true, // avoid default modal
-        },
-      };
-      setConnector(new CoinbaseWalletConnector(config));
-    }
-  }, []);
+    const coinbaseWallet = connectors.find((c) => c.id === 'coinbaseWallet');
+    if (coinbaseWallet) setConnector(coinbaseWallet);
+  }, [connectors]);
 
   return {
     connector,
