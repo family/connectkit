5f21b1f205223de2b5de11c3f115dad8803c16f5
pugson <hi@wojtek.im>
2023-02-27 04:58:12 +0100

    remove unused forwardRef from Alert component

--- packages/connectkit/src/components/Common/Alert/index.tsx
+++ packages/connectkit/src/components/Common/Alert/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { AlertProps } from './types';
 import { AlertContainer, IconContainer } from './styles';
 
-const Alert = React.forwardRef(
-  ({ children, icon }: AlertProps, ref: React.Ref<HTMLElement>) => {
-    return (
-      <AlertContainer>
-        {icon && <IconContainer>{icon}</IconContainer>}
-        <div>{children}</div>
-      </AlertContainer>
-    );
-  }
-);
+const Alert = ({ children, icon }: AlertProps) => {
+  return (
+    <AlertContainer>
+      {icon && <IconContainer>{icon}</IconContainer>}
+      <div>{children}</div>
+    </AlertContainer>
+  );
+};
 Alert.displayName = 'Alert';
 
 export default Alert;
