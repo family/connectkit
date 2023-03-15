5842550a8a85bac7301ea369aa02a60120d7902e
Lochie Axon <lochieaxon@gmail.com>
2023-03-15 23:24:41 +1100

    add hooks to get connectors

--- packages/connectkit/src/hooks/useConnectors.ts
+++ packages/connectkit/src/hooks/useConnectors.ts
@@ -1,6 +1,56 @@
-import { useConnect } from 'wagmi';
+import { Connector, useConnect } from 'wagmi';
 
 export function useConnectors() {
   const { connectors } = useConnect();
   return connectors;
+}
+
+export function useConnector(id: string) {
+  const connectors = useConnectors();
+  return connectors.find((c) => c.id === id) as Connector;
+}
+
+export function useInjectedConnector() {
+  /*
+  options: {
+    shimDisconnect: true,
+    name: (
+      detectedName: string | string[] // Detects the name of the injected wallet
+    ) =>
+      `Injected (${
+        typeof detectedName === 'string'
+          ? detectedName
+          : detectedName.join(', ')
+      })`,
+  }
+  */
+  return useConnector('injected');
+}
+export function useWalletConnectConnector() {
+  /*
+  options: {
+    qrcode: false,
+    // or
+    showQrModal: false,
+  }
+  */
+  return useConnector('walletConnect');
+}
+export function useCoinbaseWalletConnector() {
+  /*
+  options: {
+    headlessMode: true,
+  }
+  */
+  return useConnector('coinbaseWallet');
+}
+export function useMetaMaskConnector() {
+  /*
+  options: {
+    shimDisconnect: true,
+    shimChainChangedDisconnect: true,
+    UNSTABLE_shimOnConnectSelectAccount: true,
+  }
+  */
+  return useConnector('metaMask');
 }
