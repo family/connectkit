6a748e6d2e82a6a070d415d4c0d809c5e9cec8e4
Lochie Axon <lochieaxon@gmail.com>
2023-03-23 16:32:01 +1100

    localstorage abstraction

---
+++ packages/connectkit/src/utils/localstorage.ts
@@ -0,0 +1,43 @@
+/**
+ * LocalStorage helper functions to save and retrieve data on a per-app basis
+ * e.g recently connected wallets, transactions, etc.
+ */
+
+import { randomUUID } from 'crypto';
+
+export const save = (storageKey: string, data: any[]) => {
+  localStorage.setItem(storageKey, JSON.stringify(data));
+  return get(storageKey);
+};
+
+export const get = (storageKey: string) => {
+  const data = localStorage.getItem(storageKey);
+  if (data) return JSON.parse(data);
+  return [];
+};
+
+export const add = (storageKey: string, item: any) => {
+  const data = get(storageKey);
+  const newData = [
+    {
+      ...item,
+      ckStoreKey: randomUUID(), // unique key to identify the item so we can remove it later
+      timestamp: new Date(),
+    },
+    ...data,
+  ];
+  save(storageKey, newData);
+  return get(storageKey);
+};
+
+export const remove = (storageKey: string, item: any) => {
+  const data = get(storageKey);
+  const newData = data.filter((i: any) => i.ckStoreKey !== item.ckStoreKey);
+  save(storageKey, newData);
+  return get(storageKey);
+};
+
+export const clear = (storageKey: string) => {
+  save(storageKey, []);
+  return get(storageKey);
+};
