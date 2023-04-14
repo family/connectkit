010b0805244f9a9259eeca9cb11d9159ae064c1f
nheingit <60185486+nheingit@users.noreply.github.com>
2023-04-14 09:43:39 -0500

    Add Optional chaining to isPhantom Check

--- packages/connectkit/src/utils/index.ts
+++ packages/connectkit/src/utils/index.ts
@@ -129,7 +129,7 @@ const isCoinbaseWallet = () => {
 const isPhantom = () => {
   if (typeof window === 'undefined') return false;
   const { phantom } = window as any;
-  const isPhantom = Boolean(phantom.ethereum?.isPhantom);
+  const isPhantom = Boolean(phantom?.ethereum?.isPhantom);
   if (isPhantom) return true;
   return false;
 };
