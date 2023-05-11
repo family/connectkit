13dfb8352e756c24d3df53f3c71090975acb3e94
Lochie Axon <lochieaxon@gmail.com>
2023-05-11 12:31:14 +1000

    add siwe callbacks to testbench

--- examples/testbench/src/utils/siweServer.ts
+++ examples/testbench/src/utils/siweServer.ts
@@ -1,3 +1,10 @@
 import { configureServerSideSIWE } from 'connectkit-next-siwe';
 
-export const siweServer = configureServerSideSIWE({});
+export const siweServer = configureServerSideSIWE({
+  options: {
+    afterLogout: async () => console.log('afterLogout'),
+    afterNonce: async () => console.log('afterNonce'),
+    afterSession: async () => console.log('afterSession'),
+    afterVerify: async () => console.log('afterVerify'),
+  },
+});
