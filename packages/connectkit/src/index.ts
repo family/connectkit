4b72e57f66f84de25fcc4ca0fc9ec7c75e381538
Lochie Axon <lochieaxon@gmail.com>
2024-02-01 13:43:32 +1100

    package version bump

--- packages/connectkit/src/index.ts
+++ packages/connectkit/src/index.ts
@@ -1,4 +1,4 @@
-export const CONNECTKIT_VERSION = '1.6.0';
+export const CONNECTKIT_VERSION = '1.7.0';
 
 export * as Types from './types';
 export { default as getDefaultConfig } from './defaultConfig';
