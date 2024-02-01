df9a5f41dd1ea99413c27a94a84bad9e171fbf97
Lochie Axon <lochieaxon@gmail.com>
2024-02-01 14:20:57 +1100

    clean up config

--- packages/connectkit/src/defaultConfig.ts
+++ packages/connectkit/src/defaultConfig.ts
@@ -1,4 +1,4 @@
-import { type CreateConnectorFn } from 'wagmi';
+import { http } from 'wagmi';
 import { type CreateConfigParameters } from '@wagmi/core';
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 
@@ -18,37 +18,44 @@ type DefaultConfigProps = {
 
   // WC 2.0 requires a project ID (get one here: https://cloud.walletconnect.com/sign-in)
   walletConnectProjectId: string;
-} & CreateConfigParameters;
+} & Partial<CreateConfigParameters>;
 
 const defaultConfig = ({
   appName = 'ConnectKit',
   appIcon,
   appDescription,
   appUrl,
-  chains = [mainnet, polygon, optimism, arbitrum],
-  connectors,
   walletConnectProjectId,
+
+  chains = [mainnet, polygon, optimism, arbitrum],
+  client,
   ...props
 }: DefaultConfigProps): CreateConfigParameters => {
   globalAppName = appName;
   if (appIcon) globalAppIcon = appIcon;
 
-  // TODO: nice to have, automate transports based on chains
+  // TODO: nice to have, automate transports based on chains, but for now just provide public if not provided
+  const transports: CreateConfigParameters['transports'] =
+    props?.transports ??
+    Object.fromEntries(chains.map((chain) => [chain.id, http()]));
 
-  const config: CreateConfigParameters = {
+  const connectors: CreateConfigParameters['connectors'] =
+    props?.connectors ??
+    defaultConnectors({
+      app: {
+        name: appName,
+        icon: appIcon,
+        description: appDescription,
+        url: appUrl,
+      },
+      walletConnectProjectId,
+    });
+
+  const config: CreateConfigParameters<any, any> = {
     ...props,
     chains,
-    connectors:
-      connectors ??
-      defaultConnectors({
-        app: {
-          name: appName,
-          icon: appIcon,
-          description: appDescription,
-          url: appUrl,
-        },
-        walletConnectProjectId,
-      }),
+    connectors,
+    transports,
   };
 
   return config;
