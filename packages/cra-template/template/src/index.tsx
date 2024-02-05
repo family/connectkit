84c7fbd0708504c37d74247c0ea970a5b46acda5
Lochie Axon <lochieaxon@gmail.com>
2024-02-05 23:53:44 +1100

    update examples

--- packages/cra-template/template/src/index.tsx
+++ packages/cra-template/template/src/index.tsx
@@ -4,15 +4,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { WagmiProvider, createConfig } from 'wagmi';
-import { mainnet, polygon } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
 
 const config = createConfig(
   getDefaultConfig({
     appName: 'My App Name',
-    //infuraId: process.env.REACT_APP_INFURA_ID,
-    //alchemyId:  process.env.REACT_APP_ALCHEMY_ID,
-    chains: [mainnet, polygon],
     walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID!,
   })
 );
