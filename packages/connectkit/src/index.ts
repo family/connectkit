dfe41cc333b2d0815100ace155065c7f56414669
Lochie Axon <lochieaxon@gmail.com>
2024-02-06 00:08:17 +1100

    missing syntax

--- packages/connectkit/src/index.ts
+++ packages/connectkit/src/index.ts
@@ -1,4 +1,4 @@
-export const CONNECTKIT_VERSION = '1.7.1;
+export const CONNECTKIT_VERSION = '1.7.1';
 
 export * as Types from './types';
 export { default as getDefaultConfig } from './defaultConfig';
