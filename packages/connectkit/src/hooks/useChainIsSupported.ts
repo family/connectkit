264011e3f3829d000a15fe60e4df932455f32826
philogicae <38438271+philogicae@users.noreply.github.com>
2024-02-06 20:27:50 +0200

    Fix "change in the order of Hooks"

--- packages/connectkit/src/hooks/useChainIsSupported.ts
+++ packages/connectkit/src/hooks/useChainIsSupported.ts
@@ -1,7 +1,7 @@
 import { useConfig } from 'wagmi';
 
 export function useChainIsSupported(chainId?: number): boolean | null {
-  if (!chainId) return false;
   const { chains } = useConfig();
+  if (!chainId) return false;
   return chains.some((x) => x.id === chainId);
 }
