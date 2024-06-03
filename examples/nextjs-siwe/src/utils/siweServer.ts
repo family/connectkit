09da77830992c3058bae7a5781ffb3b0cfe47eb7
Lochie Axon <lochieaxon@gmail.com>
2024-06-03 23:21:24 +1000

    update packages

--- examples/nextjs-siwe/src/utils/siweServer.ts
+++ examples/nextjs-siwe/src/utils/siweServer.ts
@@ -1,14 +1,6 @@
 import { configureServerSideSIWE } from 'connectkit-next-siwe';
-import { createPublicClient, http } from 'viem';
-import { mainnet } from 'viem/chains';
-
-const publicClient = createPublicClient({
-  chain: mainnet,
-  transport: http(),
-});
 
 export const siweServer = configureServerSideSIWE({
-  publicClient,
   session: {
     cookieName: 'connectkit-next-siwe',
     password: process.env.SESSION_SECRET,
