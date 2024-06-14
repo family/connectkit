3019de1ca34c57795598341280b3142581c2a3c3
Lochie Axon <lochieaxon@gmail.com>
2024-06-14 13:38:46 +1000

    Update index.tsx

--- examples/testbench/src/pages/index.tsx
+++ examples/testbench/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   Types,
