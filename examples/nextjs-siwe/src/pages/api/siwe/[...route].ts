4b574cb01e0b4cf3c96e2b536c5b3c890e04a660
Martí Mayoral <76410086+martimayoral@users.noreply.github.com>
2025-02-05 15:58:59 +0100

    fix build

--- examples/nextjs-siwe/src/pages/api/siwe/[...route].ts
+++
@@ -1,3 +0,0 @@
-import { siweServer } from '@/utils/siweServer' // Your path to siweServer.ts
-
-export default siweServer.apiRouteHandler
--- examples/nextjs-siwe/src/utils/siweClient.ts
+++
@@ -1,6 +0,0 @@
-import { configureClientSIWE } from 'connectkit-next-siwe'
-
-export const siweClient = configureClientSIWE({
-  apiRoutePrefix: '/api/siwe', // Your API route directory
-  statement: 'Sign In With Ethereum to prove you control this wallet.', // optional
-})
