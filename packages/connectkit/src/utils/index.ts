e646840b31915927d9af1c358a6861e93944e8e2
Léo Pradel <pradel.leo@gmail.com>
2025-08-07 17:01:13 +0200

    feat: add support for base account

--- packages/connectkit/src/utils/index.ts
+++ packages/connectkit/src/utils/index.ts
@@ -92,6 +92,9 @@ export const isMetaMaskConnector = (connectorId?: string) =>
 export const isCoinbaseWalletConnector = (connectorId?: string) =>
   connectorId === 'coinbaseWalletSDK';
 
+export const isBaseAccountConnector = (connectorId?: string) =>
+  connectorId === 'baseAccount';
+
 export const isLedgerConnector = (connectorId?: string) =>
   connectorId === 'ledger';
 
