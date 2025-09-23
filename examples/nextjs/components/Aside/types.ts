85e968b67f360f1115da4c91548eb63d07677ac5
Huguet57 <andreuhuguet@gmail.com>
2025-09-23 17:29:11 +0200

    chore: delete old code

--- examples/nextjs/components/Aside/types.ts
+++
@@ -1,2 +0,0 @@
-
-export type AsideContentType = "main" | "providers"
--- examples/playground/src/components/Showcase/auth/CommonEmailContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export type CommonEmail = {
-  email?: string;
-  setEmail: (email: string) => void;
-}
-
-export const CommonEmailContext = createContext<CommonEmail | undefined>(undefined);
