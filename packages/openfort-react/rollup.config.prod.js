6e0975355bf8abf72da655a8c23d568c779ca5a7
Huguet57 <andreuhuguet@gmail.com>
2025-10-21 10:54:56 +0200

    fix: restore rollup files

---
+++ packages/openfort-react/rollup.config.prod.js
@@ -0,0 +1,23 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from 'rollup-plugin-typescript2'
+
+import packageJson from './package.json'
+
+export default [
+  {
+    input: ['./src/index.ts'],
+    external: ['react', 'react-dom', 'framer-motion', 'wagmi'],
+    output: {
+      file: packageJson.exports.import,
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      peerDepsExternal(),
+      typescript({
+        useTsconfigDeclarationDir: true,
+        exclude: 'node_modules/**',
+      }),
+    ],
+  },
+]
