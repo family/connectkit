475a1f0e8d459450c77efa2ecc8519b775380812
Martí Mayoral <76410086+martimayoral@users.noreply.github.com>
2025-11-04 13:32:11 +0100

    remove playground api

--- examples/playground/src/api/test.ts
+++
@@ -1,8 +0,0 @@
-// api/hello.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async function fetch(req: VercelRequest, res: VercelResponse) {
-  // biome-ignore lint: sample
-  console.log('Request received:', req.body)
-  res.status(200).json({ message: 'Hello from TypeScript API!' })
-}
