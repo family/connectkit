7d7c000d49a2ae9e91d1a206d6db117f1290d4d8
Martí Mayoral <76410086+martimayoral@users.noreply.github.com>
2025-11-04 13:02:14 +0100

    update playground api test

--- examples/playground/src/api/test.ts
+++ examples/playground/src/api/test.ts
@@ -1,7 +1,7 @@
 // api/hello.ts
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function fetch(req: VercelRequest, res: VercelResponse) {
   // biome-ignore lint: sample
   console.log('Request received:', req.body)
   res.status(200).json({ message: 'Hello from TypeScript API!' })
