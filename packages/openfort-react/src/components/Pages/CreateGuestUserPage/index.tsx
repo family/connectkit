e745594d2fe4ca5cbff9451085c5ec31b19e38d6
Martí Mayoral <76410086+martimayoral@users.noreply.github.com>
2025-11-18 13:10:50 +0100

    improve create guest on dev mode

--- packages/openfort-react/src/components/Pages/CreateGuestUserPage/index.tsx
+++ packages/openfort-react/src/components/Pages/CreateGuestUserPage/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useGuestAuth } from '../../../hooks/openfort/auth/useGuestAuth'
+import { logger } from '../../../utils/logger'
 import Loader from '../../Common/Loading'
 import { routes } from '../../Openfort/types'
 import { useOpenfort } from '../../Openfort/useOpenfort'
@@ -9,12 +10,16 @@ const CreateGuestUserPage = () => {
   const { setRoute } = useOpenfort()
   const { signUpGuest, error } = useGuestAuth({
     recoverWalletAutomatically: false,
-    onSuccess: () => {
-      setRoute(routes.LOAD_WALLETS)
-    },
+    onSuccess: () => setRoute(routes.LOAD_WALLETS),
   })
 
+  const initialized = useRef(false)
+
   useEffect(() => {
+    if (initialized.current) return
+    initialized.current = true
+
+    logger.log('Signing up guest user...')
     signUpGuest()
   }, [])
 
