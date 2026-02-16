54d3dc00dbc902dcc7528522342cbd97d734b248
Jaume Alavedra <jamalavedra@gmail.com>
2026-02-16 16:28:33 +0100

    chore: cleanup unused

--- examples/playground/tests/utils/constants.ts
+++ examples/playground/tests/utils/constants.ts
@@ -5,9 +5,6 @@ import { fileURLToPath } from 'node:url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// Strong password for E2E tests
-export const STRONG_PASSWORD = process.env.E2E_WALLET_PASSWORD ?? 'Openfort_E2E_2025!A9x#7Kp'
-
 // Project root
 export const ROOT_OUT = path.resolve(__dirname, '../../../..')
 
